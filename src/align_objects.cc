72f814e59583355e1693c19e071dac17df8a1819
Marko Panjek <panjekm@student.ethz.ch>
2017-10-04 20:26:37 +0200

    passing filenames of clouds you want to merge
    so that it can operate as standalone app

--- src/align_objects.cc
+++ src/align_objects.cc
@@ -1,23 +1,40 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
+#include <pcl/console/parse.h>
 #include <pcl/filters/filter.h>
 #include <pcl/io/ply_io.h>
-#include <ros/ros.h>
 
 #include "modelify/alignment_toolbox/alignment_toolbox.h"
 #include "modelify/common.h"
 #include "modelify/matching_visualizer.h"
-#include "modelify/testing_toolbox/test_fixtures.h"
 
 using namespace modelify;
 
 int main(int argc, char** argv) {
-  std::cout << "HELLO MARKO" << std::endl;
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
 
-  testing_toolbox::TestFixture test_fixture;
-  test_fixture.loadYamlConfigFile("test_data/datasets.yaml");
-  testing_toolbox::Dataset data;
-  test_fixture.loadDataset("toy_robot", &data);
+  PointSurfelCloudType::Ptr pointcloud_1(new PointSurfelCloudType);
+  PointSurfelCloudType::Ptr pointcloud_2(new PointSurfelCloudType);
+  std::vector<int> filename_indices =
+      pcl::console::parse_file_extension_argument(argc, argv, "ply");
+  if (filename_indices.size() == 3) {
+    std::string filename = argv[filename_indices[0]];
+    if (pcl::io::loadPLYFile(filename, *pointcloud_1) == -1) {
+      std::cout << "Was not able to open file " << filename << std::endl;
+      return 0;
+    }
+    filename = argv[filename_indices[1]];
+    if (pcl::io::loadPLYFile(filename, *pointcloud_2) == -1) {
+      std::cout << "Was not able to open file " << filename << ".\n";
+      return 0;
+    }
+  } else {
+    std::cout << "ERROR: no input files provided!" << std::endl;
+    std::cout << "INSTRUCTIONS: filename.ply filename2.ply output.ply"
+              << std::endl;
+    return 0;
+  }
 
   Transformation initial_guess = Eigen::Matrix4f::Identity();
   alignment_toolbox::ICPParams params;
@@ -25,9 +42,8 @@ int main(int argc, char** argv) {
   params.inlier_distance_threshold_factor = 5.0;
   params.transformation_epsilon = 1e-7;
   params.max_iterations = 1000u;
-  const double resolution =
-      std::min(computeCloudResolution(data.point_clouds[1]),
-               computeCloudResolution(data.point_clouds[0]));
+  const double resolution = std::min(computeCloudResolution(pointcloud_2),
+                                     computeCloudResolution(pointcloud_1));
 
   // Estimate transformation.
   Transformation refined_transform;
@@ -36,9 +52,8 @@ int main(int argc, char** argv) {
   std::vector<size_t> outlier_indices;
 
   alignment_toolbox::estimateTransformationPointToPoint<PointSurfelType>(
-      data.point_clouds[0], data.point_clouds[1], initial_guess, params,
-      resolution, &refined_transform, &mean_squared_error, &inlier_ratio,
-      &outlier_indices);
+      pointcloud_1, pointcloud_2, initial_guess, params, resolution,
+      &refined_transform, &mean_squared_error, &inlier_ratio, &outlier_indices);
   CHECK_LT(mean_squared_error, 5 * 1e-4);
   CHECK_GT(inlier_ratio, 0.8);
 
@@ -49,24 +64,19 @@ int main(int argc, char** argv) {
   LOG(INFO) << "refined transformation: " << refined_transform;
 
   // Appy estimated transformation.
-  pcl::transformPointCloud(*(data.point_clouds[0]), *(data.point_clouds[0]),
-                           refined_transform);
+  pcl::transformPointCloud(*(pointcloud_1), *(pointcloud_1), refined_transform);
 
   // visualize the alignment
   std::unique_ptr<MatchingVisualizer> visualizer;
   const bool kShowWindow = true;
   visualizer.reset(new MatchingVisualizer("Visualizer", kShowWindow));
-  visualizer->setFileName("test_results/aligned_clouds.png");
-  visualizer->setPointClouds(data.point_clouds[0], data.point_clouds[1]);
-  if (data.keypoints_available) {
-    visualizer->setKeyPoints(data.keypoint_clouds[0], data.keypoint_clouds[1]);
-  }
+  visualizer->setPointClouds(pointcloud_1, pointcloud_2);
   visualizer->visualize();
 
   PointSurfelCloudType::Ptr cloud_merged(new PointSurfelCloudType);
-  *cloud_merged += *data.point_clouds[0];
-  *cloud_merged += *data.point_clouds[1];
-  pcl::io::savePLYFileBinary("pointclouds_merged.ply", *cloud_merged);
+  *cloud_merged += *pointcloud_1;
+  *cloud_merged += *pointcloud_2;
+  pcl::io::savePLYFileBinary(argv[filename_indices[2]], *cloud_merged);
 
   return 1;
 }
